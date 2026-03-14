92aa578508c344e79d47e2d7130aa74db29d6605
mediaman88 <junk@jefferymail.com>
2026-03-14 11:12:02 +0800

    Update overpass.js

--- api/overpass.js
+++ api/overpass.js
@@ -1,24 +1,17 @@
-const https = require(“https”);
-
-module.exports = function handler(req, res) {
-const query = req.query.data;
-if (!query) { res.status(400).json({error:“No query”}); return; }
-
-const url = “https://overpass-api.de/api/interpreter?data=” + encodeURIComponent(query);
-
-https.get(url, function(apiRes) {
-let data = “”;
-apiRes.on(“data”, function(chunk) { data += chunk; });
-apiRes.on(“end”, function() {
-try {
-const parsed = JSON.parse(data);
-res.setHeader(“Access-Control-Allow-Origin”, “*”);
-res.status(200).json(parsed);
-} catch(e) {
-res.status(500).json({error: “Parse error”, raw: data.slice(0,200)});
-}
+var https = require(‘https’);
+module.exports = function(req, res) {
+var q = req.query.data;
+if (!q) { res.status(400).end(‘No query’); return; }
+var url = ‘https://overpass-api.de/api/interpreter?data=’ + encodeURIComponent(q);
+https.get(url, function(r) {
+var d = ‘’;
+r.on(‘data’, function(c) { d += c; });
+r.on(‘end’, function() {
+res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
+res.setHeader(‘Content-Type’, ‘application/json’);
+res.status(200).end(d);
 });
-}).on(“error”, function(e) {
-res.status(500).json({error: e.message});
+}).on(‘error’, function(e) {
+res.status(500).end(e.message);
 });
-}
+};
